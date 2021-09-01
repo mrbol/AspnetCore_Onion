a94d06599c71ac580a1e4fbd81556459514cf846
tdr <tdr.evolution@gmail.com>
2021-08-31 22:38:03 -0300

    Exemplo mplementando A Arquitetura Onion

--- OnionArchitecture/WebApi/Startup.cs
+++ OnionArchitecture/WebApi/Startup.cs
@@ -34,7 +34,19 @@ namespace WebApi
 
             });
             #endregion
-            
+
+            #region Api Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+            #endregion
             services.AddApplication();
             services.AddPersistence(Configuration);
             services.AddControllers();
